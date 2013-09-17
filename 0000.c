c89b065ef8d1ecb2c58ea17a3c4c1d9a1b62dc21
ochaochaocha3 <ochaochaocha3@gmail.com>
2013-09-17 20:26:44 +0900

    AOJ 0000 QQ

---
+++ 0000.c
@@ -0,0 +1,15 @@
+/* AOJ 0000 QQ */
+
+#include <stdio.h>
+
+int
+main(void)
+{
+    int i, j;
+
+    for (i = 1; i < 10; i++)
+        for (j = 1; j < 10; j++)
+            printf("%dx%d=%d\n", i, j, i * j);
+
+    return 0;
+}
---
+++ 0000.rb
@@ -0,0 +1,7 @@
+# AOJ 0000 QQ
+
+(1..9).each do |i|
+  (1..9).each do |j|
+    puts "#{i}x#{j}=#{i * j}"
+  end
+end
