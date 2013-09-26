3742a709eb3b1e24499cfa526f56dce4e4208acc
ochaochaocha3 <ochaochaocha3@gmail.com>
2013-09-26 20:56:22 +0900

    AOJ 0034 Railway Lines

---
+++ 0034.rb
@@ -0,0 +1,20 @@
+# AOJ 0034 Railway Lines
+
+while line = gets
+  dat = line.split(",").map(&:to_i)
+  lengths, v1, v2 = dat[0..9], dat[10], dat[11]
+
+  distances = [0]
+  lengths.each.with_index(1) do |l, i|
+    distances[i] = distances[i - 1] + l
+  end
+
+  x = v1.to_f / (v1 + v2) * lengths.reduce(0, :+)
+
+  distances.each_with_index do |d, i|
+    if x <= d
+      puts i
+      break
+    end
+  end
+end
